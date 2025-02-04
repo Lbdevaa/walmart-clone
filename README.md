1e65f2ae43b2a5e58a6b164d3eaa079c310a9644
Lebedev Andrey <lbdev.aa@gmail.com>
2025-02-05 03:15:03 +0500

    add: header

--- README.md
+++ README.md
@@ -10,4 +10,12 @@ So we will never have to worry about where does the header get lay. Rendered and
 
 The children in layout will just simply get rendered in based on which screen we're in.
 
+Whenever you need interactivity inside of a server component you need to go ahead and introduce State.
+Problem: is with server components you don't have a window object because it's rendered on the server.
+So when you're doing any sort of you know interactive work you need to go ahead and actually convert it to a client component.
+
+"use client" at the top of the file and is now this being rendered on the client.
+
 - shadcn - collection of reusable components
+
+- lucide - icon library
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,106 @@
+"use client";
+
+import {
+  Grid2X2,
+  Heart,
+  LayoutGrid,
+  Search,
+  ShoppingCart,
+  User,
+} from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { FormEvent } from "react";
 
 function Header() {
+  const router = useRouter();
+  const cart = [];
+  const total = 0;
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const input = e.currentTarget.input.value
+
+    router.push(`/search?q=${input}`);
+  };
+
   return (
-    <header>
+    <header className="flex flex-col md:flex-row bg-blue-400 items-center px-10 py-7 space-x-5">
       <Link href={"/"}>
         <Image
-          src="https://links.papareact.com/xsi"
+          src="https://i.imgur.com/5V4wehM.png"
           alt="Walmart logo"
           width={150}
           height={150}
         />
       </Link>
+      <form
+        onSubmit={handleSubmit}
+        className="flex items-center space-x-2 bg-white rounded-full w-full"
+      >
+        <input
+          type="text"
+          placeholder="Search everything..."
+          className="flex-1 px-4 rounded-l-full outline-none placeholder:text-sm"
+        />
+        <button type="submit">
+          <Search className="rounded-full h-10 px-2 w-10 bg-yellow-400 cursor-pointer" />
+        </button>
+      </form>
+      <div className="flex space-x-5 mt-5 md:mt-0">
+        <Link
+          href={"/"}
+          className="hidden xl:flex text-white font-bold items-center space-x-2 text-sm"
+        >
+          <Grid2X2 size={20} />
+          <p>Departments</p>
+        </Link>
+
+        <Link
+          href={"/"}
+          className="hidden xl:flex text-white font-bold items-center space-x-2 text-sm"
+        >
+          <LayoutGrid size={20} />
+          <p>Services</p>
+        </Link>
+
+        <Link
+          href={"/"}
+          className="flex text-white font-bold items-center space-x-2 text-sm"
+        >
+          <Heart size={20} />
+          <div>
+            <p className="text-xs font-extralight">Reorder</p>
+            <p>My Items</p>
+          </div>
+        </Link>
+
+        <Link
+          href={"/"}
+          className="flex text-white font-bold items-center space-x-2 text-sm"
+        >
+          <User size={20} />
+          <div>
+            <p className="text-xs font-extralight">Sign In</p>
+            <p>Account</p>
+          </div>
+        </Link>
+
+        <Link
+          href={"/basket"}
+          className="flex text-white font-bold items-center space-x-2 text-sm"
+        >
+          <ShoppingCart size={20} />
+          <div>
+            <p className="text-xs font-extralight">
+              {cart.length > 0 ? `${cart.length} items` : "No items"}
+            </p>
+            <p>{cart.length > 0 ? `${total}` : "0"}</p>
+          </div>
+        </Link>
+      </div>
     </header>
   );
 }
